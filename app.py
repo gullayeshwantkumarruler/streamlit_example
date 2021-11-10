30b205022c72e46395591c96cbb8055fc732c338
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2021-11-11 00:18:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,20 @@ mon_images=[]
 images=["burj_khalifa/burj_khalifa (1).jpg","burj_khalifa/burj_khalifa (2).jpg","burj_khalifa/burj_khalifa (3).jpg","burj_khalifa/burj_khalifa (4).jpg"]
 for i in images:
   image = Image.open(i).resize((100, 100))
-#   st.image(image, caption='burj_khalifa')
+  st.image(image, caption='burj_khalifa')
   mon_images.append(image)
 
 # ENDPOINT = "burj_khalifa (2).jpg"
 
 
-with st.sidebar:
-    st.header("Configuration")
-    with st.form(key="grid_reset"):
-#         n_photos = st.slider("Number of cat photos:", 1, 4, 4)
-        n_cols = st.number_input("Number of columns", 4, 4, 4)
-        st.form_submit_button(label="Reset images and layout")
-    with st.expander("About this app"):
-        st.markdown("It's about cats :cat:!")
-    st.caption("Source: https://cataas.com/#/")
+# with st.sidebar:
+#     st.header("Configuration")
+#     with st.form(key="grid_reset"):
+# #         n_photos = st.slider("Number of cat photos:", 1, 4, 4)
+#         n_cols = st.number_input("Number of columns", 4, 4, 4)
+#         st.form_submit_button(label="Reset images and layout")
+#     with st.expander("About this app"):
+#         st.markdown("It's about cats :cat:!")
 
 # st.title("Choose your favorite cat :cat:")
 # st.caption(
@@ -29,9 +28,9 @@ with st.sidebar:
 # )
 
 cat_images =mon_images
-n_rows = 1 + len(cat_images) // int(n_cols)
-rows = [st.container() for _ in range(n_rows)]
-cols_per_row = [r.columns(n_cols) for r in rows]
+n_rows = 1 + len(cat_images) // int(1.0)
+rows = [st.container() for _ in range(1.0)]
+cols_per_row = [r.columns(4.0) for r in rows]
 cols = [column for row in cols_per_row for column in row]
 
 for image_index, cat_image in enumerate(cat_images):
