c97a95cdd24d3e679775c33f1d01366c1e7cb35a
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2021-11-11 00:15:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,17 +16,17 @@ for i in images:
 with st.sidebar:
     st.header("Configuration")
     with st.form(key="grid_reset"):
-        n_photos = st.slider("Number of cat photos:", 1, 4, 4)
-        n_cols = st.number_input("Number of columns", 1, 4, 4)
+#         n_photos = st.slider("Number of cat photos:", 1, 4, 4)
+        n_cols = st.number_input("Number of columns", 4, 4, 4)
         st.form_submit_button(label="Reset images and layout")
     with st.expander("About this app"):
         st.markdown("It's about cats :cat:!")
     st.caption("Source: https://cataas.com/#/")
 
-st.title("Choose your favorite cat :cat:")
-st.caption(
-    "You can display the image in full size by hovering it and clicking the double arrow"
-)
+# st.title("Choose your favorite cat :cat:")
+# st.caption(
+#     "You can display the image in full size by hovering it and clicking the double arrow"
+# )
 
 cat_images =mon_images
 n_rows = 1 + len(cat_images) // int(n_cols)
