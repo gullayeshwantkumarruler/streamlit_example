3f1f6291f175f35d81667805f5eeb879d5490e67
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2021-11-11 00:13:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ for i in images:
 with st.sidebar:
     st.header("Configuration")
     with st.form(key="grid_reset"):
-        n_photos = st.slider("Number of cat photos:", 1, 4, 1)
+        n_photos = st.slider("Number of cat photos:", 1, 4, 4)
         n_cols = st.number_input("Number of columns", 1, 4, 4)
         st.form_submit_button(label="Reset images and layout")
     with st.expander("About this app"):
