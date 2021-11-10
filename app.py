fcb2e225a5bf32ea4d7913b14c3fe99b0e8bd5ae
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2021-11-11 00:07:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,36 +2,37 @@ import numpy as np
 import requests
 import streamlit as st
 from PIL import Image
+
+mon_images=[]
 images=["burj_khalifa/burj_khalifa (1).jpg","burj_khalifa/burj_khalifa (2).jpg","burj_khalifa/burj_khalifa (3).jpg","burj_khalifa/burj_khalifa (4).jpg"]
 for i in images:
   image = Image.open(i).resize((100, 100))
   st.image(image, caption='burj_khalifa')
+  mon_images.append(image)
 
 # ENDPOINT = "burj_khalifa (2).jpg"
 
 
-# with st.sidebar:
-#     st.header("Configuration")
-#     with st.form(key="grid_reset"):
-#         n_photos = st.slider("Number of cat photos:", 4, 8, 4)
-#         n_cols = st.number_input("Number of columns", 2, 8, 4)
-#         st.form_submit_button(label="Reset images and layout")
-#     with st.expander("About this app"):
-#         st.markdown("It's about cats :cat:!")
-#     st.caption("Source: https://cataas.com/#/")
+with st.sidebar:
+    st.header("Configuration")
+    with st.form(key="grid_reset"):
+        n_photos = st.slider("Number of cat photos:", 4, 8, 4)
+        n_cols = st.number_input("Number of columns", 2, 8, 4)
+        st.form_submit_button(label="Reset images and layout")
+    with st.expander("About this app"):
+        st.markdown("It's about cats :cat:!")
+    st.caption("Source: https://cataas.com/#/")
 
-# st.title("Choose your favorite cat :cat:")
-# st.caption(
-#     "You can display the image in full size by hovering it and clicking the double arrow"
-# )
+st.title("Choose your favorite cat :cat:")
+st.caption(
+    "You can display the image in full size by hovering it and clicking the double arrow"
+)
 
-# cat_images = [
-#     requests.get(f"{ENDPOINT}?width=1200&height=1200").content for i in range(n_photos)
-# ]
-# n_rows = 1 + len(cat_images) // int(n_cols)
-# rows = [st.container() for _ in range(n_rows)]
-# cols_per_row = [r.columns(n_cols) for r in rows]
-# cols = [column for row in cols_per_row for column in row]
+cat_images =mon_images
+n_rows = 1 + len(cat_images) // int(n_cols)
+rows = [st.container() for _ in range(n_rows)]
+cols_per_row = [r.columns(n_cols) for r in rows]
+cols = [column for row in cols_per_row for column in row]
 
-# for image_index, cat_image in enumerate(cat_images):
-#     cols[image_index].image(cat_image)
+for image_index, cat_image in enumerate(cat_images):
+    cols[image_index].image(cat_image)
